04576c59f0635c3c51d16aa33e2b02d4ac20afed
GregoryNoelMarcello <gregorynoelmarcellosoetanto_2327240045@mhs.mdp.ac.id>
2024-10-26 08:13:36 +0700

    wisata_candi_gregory
    tugas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'screens/detail_screen.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -7,7 +10,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  //
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,16 +19,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      home: DetailScreen(candi: candiList[0]),
     );
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder ();
-  }
-}
