08573d0052f7c7f1b36b18cd4c1e21d1f21c6ea5
GregoryNoelMarcello <gregorynoelmarcellosoetanto_2327240045@mhs.mdp.ac.id>
2024-11-04 12:51:35 +0700

    nganu

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/sign_in_screen.dart';
 import 'screens/detail_screen.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +20,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
+      // home : const ProfileScreen
+      // home DetailScreen(candi: candiList[0]),
+      home: SignInScreen(),
       // home: DetailScreen(candi: candiList[0]),
     );
   }
